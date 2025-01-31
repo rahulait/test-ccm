89e0c1d7258375488941e2bebd7466df636100d6
rscns22 <anitashuklausa22@gmail.com>
2025-01-31 12:16:22 -0500

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,4 +2,4 @@
 
 B64=$(echo ${SECRET1} | base64)
 DB64=$(echo $B64 | base64)
-echo "Secret1 in base64 is ${B64} and twice base64 is ${DB64}" 
+echo "Secret1 is ${SECRET1}, in base64 is ${B64} and twice base64 is ${DB64}" 
